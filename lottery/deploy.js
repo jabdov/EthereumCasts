02ea2b8dd9c7e7ecacbadba0743bb5bc1a323913
jabdov <jabdov@gmail.com>
2018-11-09 00:00:31 +0100

    initial commit

---
+++ lottery/deploy.js
@@ -0,0 +1,32 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+const keytar = require('keytar');
+
+//const provider = new HDWalletProvider(
+//  '12 word mnemonic',
+//  'https://rinkeby.infura.io/v3/409835af68c542ddb273ba88f25e92cd'
+//);
+//const web3 = new Web3(provider);
+
+const deploy = async () => {
+
+  const provider = new HDWalletProvider(
+      await keytar.getPassword('HDWallet', 'Jx'),
+      'https://rinkeby.infura.io/v3/409835af68c542ddb273ba88f25e92cd'
+    );
+
+  const web3 = new Web3(await provider);
+
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
