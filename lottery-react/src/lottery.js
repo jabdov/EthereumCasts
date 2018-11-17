799620cd19776ca23c6d0bb5c0bfff82f25739eb
jabdov <jabdov@gmail.com>
2018-11-17 22:34:12 +0100

    initial commit

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from "./web3";
+
+const address = "0xaf3299f26A3B62186f6444Fc947407B2FCB6f78f";
+
+const abi =
+  '[{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]';
+
+export default new web3.eth.Contract(JSON.parse(abi), address);
