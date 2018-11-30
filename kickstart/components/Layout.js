3bacb4564643260824d3a23022d7c9148a8ea6e4
jabdov <jabdov@gmail.com>
2018-11-30 22:55:17 +0100

    dev updates

--- kickstart/components/Layout.js
+++ kickstart/components/Layout.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
+import Header from './Header';
 
 export default props => {
   return (
-    <div>
-      <h1>I'm a header</h1>
+    <Container>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css"
+        />
+      </Head>
+
+      <Header />
       {props.children}
-      <h1>I'm a footer</h1>
-    </div>
+    </Container>
   );
 };
--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -26,14 +27,20 @@ class CampaignIndex extends Component {
     return (
       <Layout>
         <div>
-          <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css"></link>
           <h3>Open Campaigns</h3>
+          
+          <Link route="/campaigns/new">
+            <a>
+              <Button
+                floated="right"
+                content="Create Campaign"
+                icon="add"
+                primary
+              />
+            </a>
+          </Link>
+
           {this.renderCampaigns()}
-          <Button
-            content="Create Campaign"
-            icon="add"
-            primary
-          />
         </div>
       </Layout>
     );
